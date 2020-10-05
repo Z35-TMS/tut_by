a86d8e98b631be9be5168e4b3640d9db63c5df1b
Dmitriy Amelchenko <d.a.amelchenko@outlook.com>
2020-10-05 22:05:44 +0300

    blocks

--- parser/tut.py
+++ parser/tut.py
@@ -1,15 +1,21 @@
-import re
+import requests
 from bs4 import BeautifulSoup as BS
 
-
+#content-band > div.col-c > div > div.col-w > div.l-columns.air-30
 class TutBy:
-    __url = ""
+    __url = "https://news.tut.by/archive/{}.html"
 
     def __init__(self, date: str):
-        self.__news = []
+        self.__news = {}
+        self.__date = date
 
     def get_page(self):
-        return None
+        root_page = requests.get(self.__url.format(self.__date))
+        html = BS(root_page.text, features="html5lib")
+        html = html.find("div", attrs={"id": "content-band"})
+        html = html.find("div", attrs={"class": "l-columns"})
+        b_news = html.find_all("div", attrs={"class": "b-news"})
+        print(len(b_news))
 
     def parse(self):
         pass
@@ -27,16 +33,16 @@ class TutBy:
         self.__cursor = 0
         raise StopIteration
 
-    def __getitem__(self, index):
-        if isinstance(index, int):
-            return self.__news[index]
-        elif isinstance(index, str):
-            raise KeyError(index)
+    # def __getitem__(self, index):
+    #     if isinstance(index, int):
+    #         return self.__news[index]
+    #     elif isinstance(index, str):
+    #         raise KeyError(index)
 
 
 if __name__ == "__main__":
     tut = TutBy("20.09.2020")
-    tut.parse()
-    for news in tut:
-        print(news)
-    
+    # tut.parse()
+    # for news in tut:
+    #     print(news)
+    tut.get_page()
